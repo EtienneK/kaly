340b7b0b4652a563e6b13ce0317ac6c6aefae538
Etienne Koekemoer <e@ek.pe>
2020-05-10 10:02:48 +0200

    Started with registration

--- iam/account/router.js
+++ iam/account/router.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const session = require('express-session');
 const flash = require('express-flash');
+const { getInstalledPathSync } = require('get-installed-path')
 const mongoose = require('mongoose');
 const MongoStore = require('connect-mongo')(session);
-
 const passport = require('passport');
 require('./config/passport');
+const path = require('path');
+const sass = require('node-sass-middleware');
 
 const router = express.Router();
 
@@ -21,6 +23,18 @@ router.use(session({
 router.use(passport.initialize());
 router.use(passport.session());
 router.use(flash());
+router.use(sass({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public')
+}));
+
+router.use('/', express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+const bootstrapPath = getInstalledPathSync('bootstrap', { local: true });
+router.use('/js/lib', express.static(path.join(bootstrapPath, 'dist/js'), { maxAge: 31557600000 }));
+const jqueryPath = getInstalledPathSync('jquery', { local: true });
+router.use('/js/lib', express.static(path.join(jqueryPath, 'dist'), { maxAge: 31557600000 }));
+const fortawesomePath = getInstalledPathSync('@fortawesome/fontawesome-free', { local: true });
+router.use('/webfonts', express.static(path.join(fortawesomePath, 'webfonts'), { maxAge: 31557600000 }));
 
 const accountController = require('./controllers/account');
 router.get('/register', accountController.getRegister);
